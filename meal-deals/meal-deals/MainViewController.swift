7b1ed3bcff66a05c83042af46b12815ecfc68890
Jacky Chiu <jackyzhanchiu@gmail.com>
2017-09-16 12:43:08 -0400

    Setup mapview to display

--- meal-deals/meal-deals/MainViewController.swift
+++ meal-deals/meal-deals/MainViewController.swift
@@ -7,14 +7,37 @@
 //
 
 import UIKit
+import MapKit
 
 class MainViewController: UIViewController {
+	private let mapView = MKMapView()
+	private let geocoder = CLGeocoder()
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		// mapView setup
+		mapView.delegate = self
+		mapView.showsUserLocation = true
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
+		// mapView region
+		let noLocation = CLLocationCoordinate2D()
+		let viewRegion = MKCoordinateRegionMakeWithDistance(noLocation, 10, 10)
+		mapView.setRegion(viewRegion, animated: true)
+		
+		// view setup
+		view.addSubview(mapView)
+		mapView.translatesAutoresizingMaskIntoConstraints = false
+		
+		NSLayoutConstraint.activate([
+			mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+			mapView.topAnchor.constraint(equalTo: view.topAnchor),
+			mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+			mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+		])
+	}
+}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
-  }
+extension MainViewController: MKMapViewDelegate {
+	
+}

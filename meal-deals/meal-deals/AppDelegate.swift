3c867b72e9c202fe2a7e9515a3893a8137ae3583
BenEmdon <benjaminemdon@gmail.com>
2017-09-16 12:04:17 -0400

    Removed storyboards

--- meal-deals/meal-deals/AppDelegate.swift
+++ meal-deals/meal-deals/AppDelegate.swift
@@ -18,6 +18,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 		// Override point for customization after application launch.
 		FirebaseApp.configure()
+
+		window = UIWindow(frame: UIScreen.main.bounds)
+		window?.rootViewController = ViewController()
+		window?.makeKeyAndVisible()
+
 		return true
 	}
 
